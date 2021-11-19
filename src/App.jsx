d9eb2284fc4a6e137e14114ef3b99187d8b290a4
Romini's coding <74459925+dalping@users.noreply.github.com>
2021-11-19 18:53:32 +0900

    웹팩 리액트 개발환경 생성

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            리액트 생성성공!
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'webpack-setting',
+    mode: 'development', // 실서비스 : production
+    devtool: 'eval', // production : hidden-source-map
+    resolve: {
+        extensions:['.js','.jsx']
+    },
+
+    entry: { //제일 중요!
+        app: './index',
+    }, // 입력
+
+    // entry에 있는 파일을 읽고 module을 적용한 후 output
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                      targets: {
+                        browsers: ['> 1% in KR'], // browserslist
+                      },
+                      debug: true,
+                    }],
+                    '@babel/preset-react',
+                  ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ],
+            },
+        }],
+    },
+
+    plugins : [ //빌드 할때마다 실행됨
+        new RefreshWebpackPlugin()
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'), // 경로를 합쳐줌
+        filename: 'app.js',
+    }, // 출력
+    devServer: {
+        static: {directory: path.resolve(__dirname,'public')},
+        port: 8080,
+        devMiddleware: {publicPath:'/dist/'},
+        hot: true,
+    },
+};
